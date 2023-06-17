5ce305730f33e9b2ece9b563a95d014b1cc0b305
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-17 13:07:43 -0400

    feat: organizing typing for reuse

--- src/interface/taskInterface.ts
+++ src/interface/taskInterface.ts
@@ -1,13 +1,13 @@
+export interface ITaskField {
+  id?: number;
+  user?: string;
+  photo?: string | null;
+  finished?: boolean;
+  title?: string;
+  description?: string;
+}
+
 export interface IGetAllTask {
     message: string;
-    data: {
-      id: number;
-      user: string;
-      photo: string | null;
-      finished: boolean;
-      title: string;
-      description: string;
-      created_at: string;
-      updated_at: string;
-    }[];
+    data: ITaskField[]
   }
